e4c0700932b3724b4cbfa18196500daaa1bdb2c8
Riya Dadlani <riyadadlani@Riyas-MacBook-Air.local>
2026-09-20 15:46:56 +0530

    deploy

---
+++ assets/sync-interval-Cg8Phswo.js
@@ -0,0 +1,1 @@
+import{a as e}from"./error-BTdmRKoB.js";var t=15e3,n=5e3;function r(t,r){if(!(t>=5e3))throw new e(`${r} must be at least ${n}ms (got ${t}); to run without memory sync, pass \`memorySyncIntervalMs: null\` to the worker instead`)}export{n,r,t};
